046b696ad9a9a38a8228157b179af7c0c147ac2a
Anton Vozghrin <anton.vozgrin@gmail.com>
2020-11-24 15:15:07 +0200

    fix: resolve react

--- patchSharing.js
+++ patchSharing.js
@@ -2,15 +2,10 @@ const path = require("path");
 const fs = require("fs");
 const patchSharing = () => {
   const React = require("react");
-  const reactPath = path.dirname(
-    typeof __non_webpack_require__ !== "undefined"
-      ? __non_webpack_require__.resolve("react")
-      : require.resolve("react")
-  );
   const umdReact =
     process.env.NODE_ENV === "production"
-      ? path.join(reactPath, "umd/react.production.min.js")
-      : path.join(reactPath, "umd/react.development.js");
+      ? require.resolve("react/umd/react.production.min.js")
+      : require.resolve("react/umd/react.development.js");
   const stringReact = fs.readFileSync(umdReact, "utf-8");
   return React.createElement("script", {
     dangerouslySetInnerHTML: {
